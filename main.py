6471da61962247be2cefb2b35e58bbea711d3448
sanyagey <mrmishamorozov@gmail.com>
2025-06-10 13:37:10 +0300

    добавлен таймер за 5 сек

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
+import time
 questions = {'Сколько планет в солнечной системе? ':'8',
              'Столица Франции':'Париж '}
 
 score = 0
 
 for q,a in questions.items():
+    start_timer = time.time()
     user_answer = input(q + '')
+    end_time = time.time()
+    if round(end_time - start_timer,1) > 5:
+        print('Вы не уложились в таймер 5 сек')
+        user_answer = 'wrong'
+    print(f'Время ответа: {round(end_time-start_timer,1)} сек')
     if user_answer.lower() == a.lower():
         print('Правильно!')
         score += 1
